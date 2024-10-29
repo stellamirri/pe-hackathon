f19292ea40eafb84e639c6d779e398d9493a9e29
Stella Mirri <stella.mirri@etu.minesparis.psl.eu>
2024-10-29 14:33:45 +0100

    idées de trucs à faire

---
+++ idees.py
@@ -0,0 +1,4 @@
+#prendre en compte l'évolution des variables sur 5-10 ans.
+#classement global sur 1 année avec toutes les variables au même coefficient (noramlisation)
+#classement global sur 1 année avec des pondérations
+#
