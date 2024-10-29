1747329f341649262c02f9b4962109b25ae8ff32
Stella Mirri <stella.mirri@etu.minesparis.psl.eu>
2024-10-29 14:48:45 +0100

    idées mises à jour

--- idees.py
+++ idees.py
@@ -1,4 +1,11 @@
-#prendre en compte l'évolution des variables sur 5-10 ans.
-#classement global sur 1 année avec toutes les variables au même coefficient (noramlisation)
-#classement global sur 1 année avec des pondérations
-#
+# Stella : prendre en compte l'évolution des variables sur 5-10 ans -> différence du classement entre 2 années
+# Sarah : classement global sur 1 année avec toutes les variables au même coefficient (noramlisation)
+# Sarah : classement global sur 1 année avec des pondérations
+# Anais : classement par variable pour chaque pays et on somme les classements d'un seul pays pour toutes les variables et celui qui a la somme la plus basse est le mieux classé.
+# Rémi : classement par continent
+#comparer au classement qui prend que le GDP pour essayer de quantifier l'écart enter les classements
+#classements de la France
+
+
+#nouvelles variables :
+#accès faciles au soin 
